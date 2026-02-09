443cae368e0b829bab15e7e46c25ff4ba7fb4b2f
MCHK <chagrin6627@gmali.com>
2026-02-09 18:42:51 +0100

    ZAE

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import CreateOrder, {
 import Order, { loader as orderLoader } from "./features/order/Order";
 import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
+
 
 const router = createBrowserRouter([
   {
